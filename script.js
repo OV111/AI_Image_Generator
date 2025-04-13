c64fb04efdf799cc2ebd945de7f49f8c7703b834
Vahe <ohanyanv703@gmail.com>
2025-04-13 16:08:30 +0400

    vars of js

---
+++ script.js
@@ -0,0 +1,7 @@
+const darkModeBtn = document.getElementById("darkmode");
+const generateBtn = document.getElementById("generateButton");
+
+const selectModel = document.getElementById("selectModel");
+const imageCount = document.getElementById("imageCount");
+const aspectRatio = document.getElementById("aspectRatio");
+
