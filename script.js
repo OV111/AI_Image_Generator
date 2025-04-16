8028acd477f721479de035c48229950882369af7
Vahe <ohanyanv703@gmail.com>
2025-04-16 18:44:59 +0400

    dark mode function

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ const selectModel = document.getElementById("selectModel");
 const imageCount = document.getElementById("imageCount");
 const aspectRatio = document.getElementById("aspectRatio");
 
+
+darkModeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+
+    console.log("works")
+})
