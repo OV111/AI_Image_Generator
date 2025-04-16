0033d849405a557d3f73809ce1035aba88774cd0
Vahe <ohanyanv703@gmail.com>
2025-04-16 19:46:49 +0400

    prompt examples and prompt button

--- script.js
+++ script.js
@@ -61,16 +61,6 @@ promptBtn.addEventListener("click", () => {
     textArea.value = example
 });
 
-
-
-
-
-
-
-
-
-
-
 darkModeBtn.addEventListener("click", () => {
     const isDarkTheme = document.body.classList.toggle("dark-theme");
     themeToggle.querySelector("i").className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon"; 
